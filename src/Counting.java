c8c3f16caf8fc259230e697b7ee72c0a9d63d6bc
alexplohoy <alexplohoy@mail.ru>
2019-04-29 15:56:05 +0300

    Methods and Classes

--- src/Counting.java
+++ src/Counting.java
@@ -2,21 +2,59 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Counting {
-    public static void FastCounting(int n, int x) {
-        int company[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            company[i] = i + 1;
-        }
-        int y = x;
-        while (n > 1) {
-            if (y <= n) {
-                for (int i = y; i < n; i++) {
-                    company[i - 1] = company[i];
-                }
-                y += x - 1;
 
-            } else
-                if (y % n > 0) {
+    static int n;
+    static int x;
+    static String company[] = new String[n];
+
+    public static void countingDetail() {
+        // приветствие
+        Output.outputGreetings();
+
+        // регистрация игроков
+        Input.inputRegister();
+
+        // ввод считалочки
+        Output.outputCounting();
+        int x = Input.inputCounting();
+
+        // вывод всех участников
+        Output.outputConditions(company, n, x);
+
+        // выбор режима игры
+        String gameMode = "3";
+        Scanner scanGameMode = new Scanner(System.in);
+        while(!(gameMode.equals("1") || gameMode.equals("2"))) {
+            System.out.println("Как будем считать?");
+            System.out.println("1 - Все стоят кружком. Считаем от начала до конца навылет");
+            System.out.println("2 - Все стоят в ряд. Считаем каждый раз с начала ряда");
+            System.out.println("3 - Свои правила");
+            gameMode = scanGameMode.nextLine();
+            if(!(gameMode.equals("1") || gameMode.equals("2") || gameMode.equals("3"))) {
+                System.out.println("Выберите, пожалуйста, одно из предложенных значений");
+            }
+        }
+        // счет
+        if (gameMode.equals("2")) {
+            // удаление участников по считалочке из массива(счет все время с первого)
+            while (n > 1) {
+                if (x % n > 0) {
+                    for (int i = x % n; i < n; i++) {
+                        company[i - 1] = company[i];
+                    }
+                }
+                /// уменьшение массива
+                arrayCut(company);
+                n--;
+                /// вывод текущего результата
+                Output.outputTempResult(company, n);
+            }
+        } else {
+            // удаление участников по считалочке из массива(счет подряд)
+            System.out.println("----------------------------------------------------------------");
+            int y = x;
+            while (n > 1) {
+                if(y % n > 0) {
                     for (int i = y % n; i < n; i++) {
                         company[i - 1] = company[i];
                     }
@@ -24,6 +62,38 @@ public class Counting {
                 } else {
                     y = x;
                 }
+                /// уменьшение массива
+                arrayCut(company);
+                n--;
+                /// вывод текущего результата
+                Output.outputTempResult(company, n);
+            }
+        }
+        // результат
+        Output.outputResult(company, n);
+
+        // финал
+        Output.outputAgain();
+        Input.inputStartAgain();
+        Output.outputFinal();
+    }
+
+
+    public static void countingFast(int n, int x) {
+        int company[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            company[i] = i + 1;
+        }
+        int y = x;
+        while (n > 1) {
+            if(y % n > 0) {
+                for (int i = y % n; i < n; i++) {
+                    company[i - 1] = company[i];
+                }
+                y = (y % n) + x - 1;
+            } else {
+                y = x;
+            }
             int companyCopy2[] = new int[company.length - 1];
             System.arraycopy(company, 0, companyCopy2, 0, companyCopy2.length);
             company = companyCopy2;
@@ -35,109 +105,16 @@ public class Counting {
     }
 
 
-    public static void DetailCounting() {
-        // приветствие
-        Output.outputGreetings();
-        // начало игры, регистрация игроков
-            String answer = "да";
-            int n = 1;
-            int p = 1;
-            String company[] = new String[n];
-            while(answer.equals("да") || answer.equals("Да") || answer.equals("ага") || answer.equals("Ага") || answer.equals("будут") || answer.equals("Будут") || answer.equals("конечно") || answer.equals("Конечно") || answer.equals("возможно") || answer.equals("") || answer.equals("1") || answer.equals("y") || answer.equals("Y") || answer.equals("yes") || answer.equals("Yes") || answer.equals("lf")) {
-                if(p <= n) {
-                    Scanner scanName = new Scanner(System.in);
-                    System.out.println("Введите имя " + p + " игрока: ");
-
-//                    System.out.println("размер n: " + n);
-//                    System.out.println("");
-//                    System.out.println("размер массива: " + company.length);
-
-                    company[(n-1)] = scanName.nextLine();
-                    while(company[(n-1)].equals("") || company[(n-1)].equals(" ")) {
-                        System.out.println("Стоит ввести хоть какое-то имя или псевдоним :)");
-                        System.out.println("Введите имя " + p + " игрока: ");
-                        company[(n-1)] = scanName.nextLine();
-                    }
-                    Scanner scanAnswer = new Scanner(System.in);
-                    System.out.println("Будут ли еще игроки?");
-                    answer = scanAnswer.nextLine();
-                    p++;
-                } else {
-                    String ArrayCopy[] = Arrays.copyOf(company, company.length + 1);
-                    company = ArrayCopy;
-                    n++;
-
-                }
+    public static int countWords(String counting) {
+        int x = 1;
+        char ch[] = counting.toCharArray();
+        for(int i = 0; i < ch.length; i++) {
+            if(ch[i] == (' ')) {
+                x++;
             }
-            if(!(answer.equals("нет") || answer.equals("Нет") || answer.equals("не") || answer.equals("Не") || answer.equals("не будет") || answer.equals("Не будет") || answer.equals("не будут") || answer.equals("Не будут") || answer.equals("больше нет") || answer.equals("Больше нет") || answer.equals("0") || answer.equals("n") || answer.equals("N") || answer.equals("no") || answer.equals("No") || answer.equals("больше не будет") || answer.equals("Больше не будет") || answer.equals("ytn") || answer.equals("все"))) {
-                System.out.println("Не совсем понятен ответ, ну да ладно..");
-            }
-
-
-            // ввод считалочки
-            Output.outputCounting();
-            int x = Input.inputCounting();
-
-            // вывод всех участников
-            Output.outputConditions(company, n, x);
-
-            // выбор режима игры
-            String gameMode = "3";
-            Scanner scanGameMode = new Scanner(System.in);
-            while(!(gameMode.equals("1") || gameMode.equals("2"))) {
-                System.out.println("Как будем считать?");
-                System.out.println("1 - Все стоят кружком. Считаем от начала до конца навылет");
-                System.out.println("2 - Все стоят в ряд. Считаем каждый раз с начала ряда");
-                System.out.println("3 - Свои правила");
-                gameMode = scanGameMode.nextLine();
-                if(!(gameMode.equals("1") || gameMode.equals("2") || gameMode.equals("3"))) {
-                    System.out.println("Выберите, пожалуйста, одно из предложенных значений");
-                }
-            }
-            // счет
-            if (gameMode.equals("2")) {
-                // удаление участников по считалочке из массива(счет все время с первого)
-                while (n > 1) {
-                    if (x % n > 0) {
-                        for (int i = x % n; i < n; i++) {
-                            company[i - 1] = company[i];
-                        }
-                    }
-                    /// уменьшение массива
-                    arrayCut(company);
-                    n--;
-                    /// вывод текущего результата
-                    Output.outputTempResult(company, n);
-                }
-            } else {
-                // удаление участников по считалочке из массива(счет подряд)
-                System.out.println("----------------------------------------------------------------");
-                int y = x;
-                while (n > 1) {
-                    if(y % n > 0) {
-                        for (int i = y % n; i < n; i++) {
-                            company[i - 1] = company[i];
-                        }
-                        y = (y % n) + x - 1;
-                    } else {
-                        y = x;
-                    }
-                    /// уменьшение массива
-                    arrayCut(company);
-                    n--;
-                    /// вывод текущего результата
-                    Output.outputTempResult(company, n);
-                }
-            }
-            // вывод оставшегося участника
-            Output.outputResult(company, n);
-
-            // финишное предложение
-            Output.outputAgain();
-            Input.inputStartAgain();
-            Output.outputFinal();
         }
-
+        return x;
+    }
 
     public static void Start() {
         String Mode = "";
@@ -152,7 +129,7 @@ public class Counting {
             }
         }
         if(Mode.equals("2")) {
-            Counting.DetailCounting();
+            Counting.countingDetail();
         } else  {
             Scanner scanNumber = new Scanner(System.in);
             System.out.println("Введите количество участников: ");
@@ -172,7 +149,7 @@ public class Counting {
                 System.out.println("Количество слов стоит вводить числом");
             }
             int x = Integer.parseInt(words);
-            Counting.FastCounting(n, x);
+            Counting.countingFast(n, x);
         }
     }
 
@@ -187,16 +164,6 @@ public class Counting {
         ArrayName = ArrayCopy;
     }
 
-    public static int countWords(String counting) {
-        int x = 1;
-        char ch[] = counting.toCharArray();
-        for(int i = 0; i < ch.length; i++) {
-            if(ch[i] == (' ')) {
-                x++;
-            }
-        }
-        return x;
-    }
 
     public static boolean positive(String tryPositive) {
         if (!(Arrays.asList(Input.positive).contains(tryPositive.toLowerCase()))) {
@@ -205,7 +172,6 @@ public class Counting {
                 String ArrayCopy[] = Arrays.copyOf(Input.positive, Input.positive.length + 1);
                 Input.positive = ArrayCopy;
                 Input.positive[Input.positive.length - 1] = tryPositive.toLowerCase();
-                return true;
             } else {
                 return false;
             }
--- src/Input.java
+++ src/Input.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Input {
 
-    static String[] positive = new String[]{"да", "давай", "ок", "ага", "угу", "конечно", "можно", "сыграем", "верно", "точно", "правильно", "y", "yes", "ok", "lf"};
+    static int n = 1;
+    static int p = 1;
+    static String answer = "";
+    static String company[] = new String[n];
+
+    static String[] positive = new String[]{"да", "давай", "ок", "ага", "угу", "конечно", "можно", "сыграем", "верно", "точно", "правильно", "хорошо", "y", "yes", "ok", "lf", "1"};
+    static String[] negative = new String[]{"нет", "не", "неа", "хватит", "хорош", "yt", "ytn", "неверно", "0"};
 
     public static String inputTestAnswer() {
-        String answer = "";
         while (!(answer.equals("1") || (answer.equals("2")))) {
             Output.outputAnswer();
             Scanner scanYes = new Scanner(System.in);
@@ -17,29 +23,60 @@ public class Input {
         return answer;
     }
 
-    public static boolean inputAnswer() {
-        Scanner scanAgain = new Scanner(System.in);
-        String again = scanAgain.nextLine();
-        if(!(Counting.positive(again))) {
-            return false;
+    public static void inputRegister() {
+        if (p <= n) {
+            inputName();
+            while (company[(n - 1)].equals("") || company[(n - 1)].equals(" ")) {
+                System.out.println("Стоит ввести хоть какое-то имя или псевдоним :)");
+                inputName();
+            }
+            System.out.println("Будут ли еще игроки?");
+            Scanner scanAnswer = new Scanner(System.in);
+            answer = scanAnswer.nextLine();
+            p++;
+        } else {
+            String ArrayCopy[] = Arrays.copyOf(company, company.length + 1);
+            company = ArrayCopy;
+            n++;
+
         }
-        return true;
+        if (Arrays.asList(Input.negative).contains(answer.toLowerCase()) && !(Counting.positive(answer))) {
+            System.out.println("Не совсем понятен ответ, ну да ладно..");
+        }
     }
 
-    public static int inputCounting() {
+    public static void inputName() {
+            System.out.println("Введите имя " + p + " игрока: ");
+            Scanner scanName = new Scanner(System.in);
+            company[(n - 1)] = scanName.nextLine();
+    }
+
+    public static void inputNewCounting() {
         Scanner scanLine = new Scanner(System.in);
         String counting = scanLine.nextLine();
         int x = Counting.countWords(counting);
         System.out.println("Кол-во слов в считалочке: " + x + ". Верно?");
-        if(!(inputAnswer())) {
-            System.out.println("Введите еще разок Вашу считалочку(не забывайте ставить пробел между словами): ");
-            inputCounting();
-        }
+        Scanner scanAgain = new Scanner(System.in);
+        String again = scanAgain.nextLine();
+    }
+
+    public static int inputCounting() {
+        int x = 1;
+        String again = "";
+        inputNewCounting();
+        do {
+            if (Arrays.asList(negative).contains(again.toLowerCase()) && !Counting.positive(again)) {
+                System.out.println("Введите еще разок Вашу считалочку(не забывайте ставить пробел между словами): ");
+                inputNewCounting();
+            }
+        } while (!Counting.positive(again));
         return x;
     }
 
     public static void inputStartAgain() {
-        if(inputAnswer()) {
+        Scanner scanAgain = new Scanner(System.in);
+        String again = scanAgain.nextLine();
+        if (!(Arrays.asList(negative).contains(again.toLowerCase())) && Counting.positive(again)) {
             Counting.Start();
         }
     }
